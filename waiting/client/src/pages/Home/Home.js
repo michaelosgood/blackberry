a216fcf35122c3914c0404c19dab782d6d7c19b0
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-11-12 16:56:50 -0800

    intitial commit

---
+++ waiting/client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import "./Home.css";
+
+const Home = () =>
+<div className="container-fluid">
+    <h1>WAITING</h1>
+    <h4>Welcome to a new dining experience!</h4>
+</div>
+export default Home;
